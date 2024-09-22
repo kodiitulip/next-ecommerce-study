52e1183432f6b11705b5c0db4bf709d8fd3e5f70
kodiitulip <kodii.tulip@gmail.com>
2024-09-22 17:04:09 -0300

    NextConfigs and page setups

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'picsum.photos',
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'ui-avatars.com',
+			},
+		],
+	},
+};
 
 export default nextConfig;
